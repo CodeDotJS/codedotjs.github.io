465e26e7dc6c3d7309f5b2bbb9e07c28f4fd471c
Rishi Giri <rushforlinux@gmail.com>
2016-12-19 09:23:24 +0530

    test

---
+++ say/js/index.js
@@ -0,0 +1,51 @@
+// function([string1, string2],target id,[color1,color2])    
+ consoleText(['Actually, I wanted to say something', 'and I think I should not.', 'But but but', 'I miss you when you are not here', 'and', 'your emoji.', 'Last...', 'Be safe and', 'be beautiful, sweetheart!', 'This is just for the OAO Mabast,', 'to whom', 'I love irritating', 'everyday.', 'And....', 'sorry', 'I lied about the dream I saw.', 'Becuase...', 'I can never use those words for you.', '🌟 💙 🕯 ∞', '^_^' ], 'text',['tomato','rebeccapurple','lightblue']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 90)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
